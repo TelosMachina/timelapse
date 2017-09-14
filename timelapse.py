8c374c229d6df9b39c4c7bfe472b01ceb4d3830b
TelosMachina <evanharmon1@gmail.com>
2017-09-13 22:04:19 -0500

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,12 +1,16 @@
 from picamera import PiCamera
 from time import sleep
+import datetime
+from os import system
 
 camera = PiCamera()
+
 
 camera.rotation = 0
 camera.resolution = (2592, 1944)
 camera.start_preview()
 for i in range(500):
-    sleep(30)
-    camera.capture('/home/pi/Desktop/images/image%s.jpg' % i)
+    sleep(15)
+    datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    camera.capture('/home/pi/Desktop/images/image%s.pg' %datetime)
 camera.stop_preview()
